0720cbc637086b83bf46119b429dd31fb9a0d4ac
danang arif <danangarif202@gmail.com>
2025-09-10 14:09:57 +0700

    update unit test and pdf viewer

--- src/feature/landing/landing.function.test.tsx
+++ src/feature/landing/landing.function.test.tsx
@@ -1,20 +1,26 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, queryByAttribute } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import HomePage from "@/pages/home/HomePage";
-import * as ReactRouterDom from 'react-router-dom';
 
-jest.mock('react-router-dom', () => ({
-  ...(jest.requireActual('react-router-dom') as object),
-  useNavigate: jest.fn(),
-}));
+const getById = queryByAttribute.bind(null, 'id');
 const mockedUsedNavigate = jest.fn();
 
-describe('HomePage', () => {
-  it('should navigate to a new route on button click', () => {
+jest.mock('react-router-dom', () => ({
+   ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedUsedNavigate,
+}));
+
+describe('Landing Page', () => {
+  it('routing button "Daftar sekarang"', () => {
     render(<HomePage />);
-    const button = screen.getByRole('button', { name: 'landing.registerNow' }); // Assuming a button with this text
+    const buttonRegister = screen.getByRole('button', { name: 'landing.registerNow' });
+    fireEvent.click(buttonRegister);
+    expect(mockedUsedNavigate).toHaveBeenCalledWith('/products');
+  });
+  it('routing button "Check RIPLAY Umum"', () => {
+    render(<HomePage />);
+    const button = screen.getByRole('button', { name: 'landing.checkRiplay' });
     fireEvent.click(button);
-    // expect(mockedUsedNavigate).toHaveBeenCalledWith('/dashboard');
-    // expect(button).toBeInTheDocument();
+    expect(mockedUsedNavigate).toHaveBeenCalledWith('/pdf/check-replay');
   });
 });
