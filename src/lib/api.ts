d77db7a25a5895444e0521c99025337e8420605e
danang arif <danangarif202@gmail.com>
2025-09-20 12:18:46 +0700

    add Idempotency-Key generate by uuid

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,16 +1,28 @@
 import axios, { type InternalAxiosRequestConfig, type AxiosHeaders } from "axios";
+import { v4 as uuidv4 } from "uuid";
+
+function getIdempotencyKey() {
+  let key = sessionStorage.getItem("idempotencyKey");
+  if (!key) {
+    key = uuidv4();
+    sessionStorage.setItem("idempotencyKey", key);
+  }
+  return key;
+}
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
-    headers: { "Content-Type": "application/json" },
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: { "Content-Type": "application/json" },
 });
 
 api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
-    const key = import.meta.env.VITE_API_KEY;
-    if (key) {
-        const h = config.headers as AxiosHeaders | Record<string, string>;
-        (h as AxiosHeaders).set?.("API-KEY", key);
-        (h as Record<string, string>)["API-KEY"] = key;
-    }
-    return config;
+  const idempotencyKey = getIdempotencyKey();
+  const key = import.meta.env.VITE_API_KEY;
+
+  if (config.headers) {
+    const h = config.headers as AxiosHeaders | Record<string, string>;
+    (h as AxiosHeaders).set?.("Idempotency-Key", idempotencyKey);
+    (h as AxiosHeaders).set?.("API-KEY", key);
+  }
+  return config;
 });
